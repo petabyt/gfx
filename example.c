31d0ee5a3883d58cfa7f7fe9e7b4f1b272f2203f
Petabyte <brikbusters@gmail.com>
2020-05-11 12:04:44 -0400

    Update example.c

--- example.c
+++ example.c
@@ -1,37 +1,26 @@
-/*
-A simple example of using the gfx library.
-CSE 20211
-9/7/2011
-by Prof. Thain
-*/
-
 #include <stdio.h>
 #include "gfx.h"
 
-int main()
-{
-	int ysize = 300;
-	int xsize = 300;
-
+int main() {
 	char c;
 
 	// Open a new window for drawing.
-	gfx_open(xsize,ysize,"Example Graphics Program");
+	gfxOpen(500 ,500, "Testing example");
 
 	// Set the current drawing color to green.
-	gfx_color(0,200,100);
+	gfxColor(0, 50, 50);
 
 	// Draw a triangle on the screen.
-	gfx_line(100,100,200,100);
-	gfx_line(200,100,150,150);
-	gfx_line(150,150,100,100);
+	gfxLine(100, 100, 200, 100);
+	gfxLine(200, 100, 150, 150);
+	gfxLine(150, 150, 100, 100);
 
 	while(1) {
 		// Wait for the user to press a character.
-		c = gfx_wait();
+		c = gfxWait();
 
 		// Quit if it is the letter q.
-		if(c=='q') break;
+		if(c == 'q') break;
 	}
 
 	return 0;
